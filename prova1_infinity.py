0494cbf372e6700f1f1d9c902841d04758aa1463
leofelipevb <leofelipevb@gmail.com>
2025-07-30 14:10:30 -0300

    primeiro commit

---
+++ prova1_infinity.py
@@ -0,0 +1,4 @@
+base = float(input("Escreva a medida da base do triangulo: "))
+altura = float(input("Escreva a medida da altura do triangulo: "))
+area_triangulo = base * altura
+print(f"A area do triangulo é {area_triangulo}!")

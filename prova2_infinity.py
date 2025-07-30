fc28ed4c0dfd9a2569d403e3f3e87319d5622002
leofelipevb <leofelipevb@gmail.com>
2025-07-30 18:46:25 -0300

    Add files via upload

---
+++ prova2_infinity.py
@@ -0,0 +1,7 @@
+numero = float(input("Digite um número: "))
+if numero < 0:
+    print(f"O número {numero} é negativo.")
+elif numero == 0:
+    print(f"O número {numero} é zero.")
+elif numero > 0:
+    print(f"O número {numero} é positivo.")
